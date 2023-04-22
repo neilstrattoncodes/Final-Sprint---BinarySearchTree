1059f0a16501461a4952d52e255c5032a4e4397d
neilstrattoncodes <95373851+neilstrattoncodes@users.noreply.github.com>
2023-04-21 23:54:48 -0230

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,37 @@
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+
+        int[] ints = {1, 2, 4, 5, 7, 9, 11};
+
+        System.out.println(" the number is " + binarySearch(ints, 9));
+    }
+
+
+    private static int binarySearch(int[] numbers, int numberToFind) {
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while (low <= high) {
+            int middlePosition = (low + high) / 2;
+            int middleNumber = numbers[middlePosition];
+
+            if (numberToFind == middleNumber) {
+                return middlePosition;
+
+            }
+            if (numberToFind < middleNumber) {
+                high = middlePosition - 1;
+            } else {
+                low = middlePosition + 1;
+            }
+        }
+        return -1;
+
+    }
+}
+
+
+
+
+
